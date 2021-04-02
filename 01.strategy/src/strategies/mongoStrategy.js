c5a18f7961ece493c54b53af12378218131194fc
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-04-02 18:42:07 -0300

    feat: create mongo and postgress strategies

---
+++ 01.strategy/src/strategies/mongoStrategy.js
@@ -0,0 +1,14 @@
+export default class MongoDBStrategy  {
+    constructor(dbStrategy) {
+        this.dbStrategy = dbStrategy
+    }
+
+    async connect() {
+     }
+
+    async create(item) {
+     }
+
+    async read(item) {
+     }
+}
---
+++ 01.strategy/src/strategies/postgressStrategy.js
@@ -0,0 +1,15 @@
+export default class PostgressDBStrategy  {
+    constructor(dbStrategy) {
+        this.dbStrategy = dbStrategy
+    }
+
+    async connect() {
+        console.log("connected")
+    }
+
+    async create(item) {
+     }
+
+    async read(item) {
+     }
+}
