cdc245dac0d036ffb8fad607346b6bfea6478bee
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-04-10 18:05:09 -0300

    feat: add event Payment

---
+++ behavioral/02.observer/src/events/payment.js
@@ -0,0 +1,13 @@
+export default class Payment {
+
+  constructor(paymentSubject) {
+    this.paymentSubject = paymentSubject;
+  }
+
+  creditCard(paymentData) {
+    console.log(`\na payment ocurred from ${paymentData.userName}`)
+    this.paymentSubject.notify(paymentData);
+  }
+
+}
+
